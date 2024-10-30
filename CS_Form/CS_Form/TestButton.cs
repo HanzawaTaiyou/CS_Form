caf8c1fc0b1e4bc1fc74a175e46cb14375eb7d42
半澤大耀 <taiyou060205@gmail.com>
2024-10-30 12:20:05 +0900

    C# ボタンと結果表示ウィンドウを追加

---
+++ CS_Form/CS_Form/TestButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CS_Form
+{
+    class TestButton : Button
+    {
+
+        ///<summary>
+        ///コンストラクタ
+        ///クラスを生成したときに呼び出される
+        ///</summary>
+        public TestButton()
+        {
+            //ClickイベントにOnClick関数を登録
+            Click += OnClick;
+        }
+
+        public void OnClick(object sender, EventArgs s)
+        {
+            MessageBox.Show("・ω・");
+        }
+
+    }
+}
