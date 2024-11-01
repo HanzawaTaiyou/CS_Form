719eaf68f33fd1eaf29e1398498b32440e9acbe5
HanzawaTaiyou <zeal24360017@fsgcl.jp>
2024-11-01 16:07:33 +0900

    CS_Form 内容 編集 ボタン10個　表示

--- CS_Form/CS_Form/Form1.cs
+++ CS_Form/CS_Form/Form1.cs
@@ -16,9 +16,38 @@ namespace CS_Form
         {
             InitializeComponent();
 
-            TestButton testButton = new TestButton();
+            // aはxの値指定
+            // bはyの値指定
+            int a = 0;
+            int b = 0;
+            int c = 0;
 
-            Controls.Add(testButton);
+            // xを100ずつ＋の方向にずらす
+            for (int i = 0; i < 3; i++)
+            {
+                // 列が移動したらyを初期化する
+                b = 0;
+
+                // yを＋の方向に100ずつずらす
+                for (int j = 0; j < 4; j++)
+                {
+                    // xが100ではないかつyが300ではない又はxが100の場合
+                    if (i != 1 && j < 3 || i == 1)
+                    {
+                        TestButton testButton = new TestButton(c,a, b, 100, 100);
+                        Controls.Add(testButton);
+
+                        c = c + 1;
+                    }
+                    // yを100ずらす
+                    b = b + 100;
+                    
+                }
+                // xを100ずらす
+                a = a + 100;
+                
+            }
+
 
         }
     }
--- CS_Form/CS_Form/TestButton.cs
+++ CS_Form/CS_Form/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,21 @@ namespace CS_Form
         ///コンストラクタ
         ///クラスを生成したときに呼び出される
         ///</summary>
-        public TestButton()
+        public TestButton(int id,int x, int y, int width, int height)
         {
             //ClickイベントにOnClick関数を登録
             Click += OnClick;
+            //ボタン内に文字を反映させる
+            Text = id.ToString();
+
+            Location = new Point(x,y);
+
+            Size = new Size(width,height);
         }
 
         public void OnClick(object sender, EventArgs s)
         {
-            MessageBox.Show("・ω・");
+            MessageBox.Show(Text);
         }
 
     }
